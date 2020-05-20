2f41abf956edd79d4242842f7c2c6dc593836dcc
Gareth Jones <garethjns4@gmail.com>
2020-05-20 20:52:01 +0100

    Add AgentExperiment, pickle compatibility

--- scripts/run_dqn_q_learner_cart_pole.py
+++ scripts/run_dqn_q_learner_cart_pole.py
@@ -1,32 +1,22 @@
-"""Train and few LinearQAgents, plot the results, and run an episode on the best agent."""
-
-import tensorflow as tf
-from joblib.externals.loky.process_executor import TerminatedWorkerError
+"""Train and few DeepQAgents, plot the results, and run an episode on the best agent."""
 
 from agents.cart_pole.q_learning.deep_q_learning_agent import DQNAgent
-from scripts.run_linear_q_learner_cart_pole import train_all, plot_all, play_best
+
+from experiment.agent_experiment import AgentExperiment
+
+
+def run_exp(n_episodes: int = 1000, max_episode_steps: int = 500):
+    DQNAgent.set_tf(512)
+
+    exp = AgentExperiment(env_spec="CartPole-v0",
+                          agent_class=DQNAgent,
+                          n_reps=5,
+                          n_jobs=1,
+                          n_episodes=n_episodes,
+                          max_episode_steps=max_episode_steps)
+
+    exp.run()
+
 
 if __name__ == "__main__":
-    try:
-        # Handle running on GPU: If available, reduce memory commitment to avoid over-committing error in 2.2.0 and
-        # for also for convenience.
-        tf.config.experimental.set_virtual_device_configuration(tf.config.experimental.list_physical_devices('GPU')[0],
-                                                                [tf.config.experimental.VirtualDeviceConfiguration(
-                                                                    memory_limit=1024)])
-    except (IndexError, AttributeError):
-        pass
-
-    common_kwargs = {"agent_class": DQNAgent,
-                     "n_agents": 10,
-                     "n_episodes": 1000,
-                     "max_episode_steps": 500}
-    try:
-        # Try and run with n_jobs > 1. Might work, probably won't. If running on GPU it'll probably break. If running
-        # on CPU it'll work if the compiled Keras models can be pickled (they probably can't).
-        agents_ = train_all(n_jobs=4, **common_kwargs)
-    except (TerminatedWorkerError, TypeError):
-        # Give up and run in slow mode.
-        agents_ = train_all(n_jobs=1, **common_kwargs)
-
-    plot_all(agents_)
-    play_best(agents_)
+    run_exp(n_episodes=500)
--- scripts/run_linear_q_learner_cart_pole.py
+++ scripts/run_linear_q_learner_cart_pole.py
@@ -1,72 +1,20 @@
 """Train and few LinearQAgents, plot the results, and run an episode on the best agent."""
-import warnings
-from typing import Callable
-from typing import List
-
-import gym
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
-from joblib import Parallel, delayed
 
 from agents.cart_pole.q_learning.linear_q_learning_agent import LinearQLearningAgent
 
-
-def fit_agent(agent_class: Callable,
-              n_episodes: int = 500, max_episode_steps: int = 500):
-    with warnings.catch_warnings():
-        warnings.simplefilter('ignore', FutureWarning)
-
-        env = gym.make("CartPole-v0")
-        agent = agent_class(env=env)
-        agent.train(n_episodes=n_episodes, max_episode_steps=max_episode_steps, verbose=False, render=False)
-
-    return agent
+from experiment.agent_experiment import AgentExperiment
 
 
-def train_all(agent_class: Callable,
-              n_agents: int = 5, n_jobs: int = -2,
-              n_episodes: int = 500, max_episode_steps: int = 500):
-    agents = Parallel(backend='loky',
-                      verbose=10,
-                      n_jobs=n_jobs)(delayed(fit_agent)(agent_class, n_episodes, max_episode_steps)
-                                     for _ in range(n_agents))
+def run_exp(n_episodes: int = 1000, max_episode_steps: int = 500):
+    exp = AgentExperiment(env_spec="CartPole-v0",
+                          agent_class=LinearQLearningAgent,
+                          n_reps=10,
+                          n_jobs=6,
+                          n_episodes=n_episodes,
+                          max_episode_steps=max_episode_steps)
 
-    return agents
-
-
-def plot_all(agents: List[LinearQLearningAgent]) -> None:
-    hist = np.hstack([np.vstack(a.history.history) for a in agents])
-
-    sns.set()
-    y_mean = np.mean(hist, axis=1)
-    y_std = np.std(hist, axis=1)
-    plt.plot(y_mean)
-    plt.fill_between(range(len(y_mean)),
-                     [max(0, s) for s in y_mean - y_std],
-                     [min(500, s) for s in y_mean + y_std],
-                     color='lightgray')
-    plt.title(f'{agents[0].name}', fontweight='bold')
-    plt.xlabel('N episodes', fontweight='bold')
-    plt.ylabel('Score', fontweight='bold')
-    plt.tight_layout()
-    plt.savefig(f'{agents[0].name}.png')
-
-
-def play_best(agents: List[LinearQLearningAgent]):
-    scores = [a.history.current_performance for a in agents]
-    best_agent = agents[int(np.argmax(scores))]
-
-    best_agent.env = gym.wrappers.Monitor(best_agent.env, 'monitor_dir', force=True)
-    best_agent.play_episode(training=False, render=False, max_episode_steps=100)
+    exp.run()
 
 
 if __name__ == "__main__":
-    agents_ = train_all(LinearQLearningAgent,
-                        n_agents=8,
-                        n_jobs=4,
-                        n_episodes=200,
-                        max_episode_steps=500)
-
-    plot_all(agents_)
-    play_best(agents_)
+    run_exp()
