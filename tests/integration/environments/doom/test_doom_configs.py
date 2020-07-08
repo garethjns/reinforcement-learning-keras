0f81a6cedaf06f2799981eb23b5882b4ab8b111a
Gareth <garethgithub@gmail.com>
2020-07-08 22:22:56 +0100

    Rearrange imports

--- tests/integration/environments/doom/test_doom_configs.py
+++ tests/integration/environments/doom/test_doom_configs.py
@@ -1,153 +1,135 @@
 import unittest
+from unittest.mock import MagicMock
+
 
 from tests.integration.environments.atari.pong.test_pong_config import TestPongStackEnvironment, TestPongDiffEnvironment
 
 try:
-    from reinforcement_learning_keras.enviroments.doom.vizdoom_basic_config import VizDoomBasicConfig
-    from reinforcement_learning_keras.enviroments.doom.vizdoom_corridor_config import VizDoomCorridorConfig
-    from reinforcement_learning_keras.enviroments.doom.vizdoom_deathmatch_config import VizDoomDeathmatchConfig
-    from reinforcement_learning_keras.enviroments.doom.vizdoom_defend_center_config import VizDoomDefendCenterConfig
-    from reinforcement_learning_keras.enviroments.doom.vizdoom_defend_line_config import VizDoomDefendLineConfig
-    from reinforcement_learning_keras.enviroments.doom.vizdoom_health_gathering_config import \
-        VizDoomHealthGatheringConfig
-    from reinforcement_learning_keras.enviroments.doom.vizdoom_health_gathering_supreme import \
-        VizDoomHealthGatheringSupremeConfig
-    from reinforcement_learning_keras.enviroments.doom.vizdoom_my_way_home_config import VizDoomMyWayHomeConfig
-    from reinforcement_learning_keras.enviroments.doom.vizdoom_predict_position_config import \
-        VizDoomPredictPositionConfig
-    from reinforcement_learning_keras.enviroments.doom.vizdoom_take_cover_config import VizDoomTakeCoverConfig
-
+    import reinforcement_learning_keras.enviroments.doom as doom
     run_tests = True
 except ImportError:
     run_tests = False
-
-
-    class DoomConfig:
-        wrapped_env = None
-
-        def __init__(self, *args, **kwargs):
-            """Dummy as real failed import and it's used in test defs."""
-            pass
+    doom = MagicMock()
 
 
 @unittest.skipUnless(run_tests, reason='ViZDoomGym not installed')
 class TestDoomStackEnvironments(TestPongStackEnvironment):
-    _sut = VizDoomBasicConfig(mode='stack', agent_type='dqn').wrapped_env
+    _sut = doom.VizDoomBasicConfig(mode='stack', agent_type='dqn').wrapped_env
     _expected_shape = (96, 128, 3)
 
 
 @unittest.skipUnless(run_tests, reason='ViZDoomGym not installed')
 class TestDoomStackEnvironments(TestPongStackEnvironment):
-    _sut = VizDoomCorridorConfig(mode='stack', agent_type='dqn').wrapped_env
+    _sut = doom.VizDoomCorridorConfig(mode='stack', agent_type='dqn').wrapped_env
     _expected_shape = (96, 128, 3)
 
 
 @unittest.skipUnless(run_tests, reason='ViZDoomGym not installed')
 class TestDoomStackEnvironments(TestPongStackEnvironment):
-    _sut = VizDoomDeathmatchConfig(mode='stack', agent_type='dqn').wrapped_env
+    _sut = doom.VizDoomDeathmatchConfig(mode='stack', agent_type='dqn').wrapped_env
     _expected_shape = (96, 128, 3)
 
 
 @unittest.skipUnless(run_tests, reason='ViZDoomGym not installed')
 class TestDoomStackEnvironments(TestPongStackEnvironment):
-    _sut = VizDoomDefendCenterConfig(mode='stack', agent_type='dqn').wrapped_env
+    _sut = doom.VizDoomDefendCenterConfig(mode='stack', agent_type='dqn').wrapped_env
     _expected_shape = (96, 128, 3)
 
 
 @unittest.skipUnless(run_tests, reason='ViZDoomGym not installed')
 class TestDoomStackEnvironments(TestPongStackEnvironment):
-    _sut = VizDoomDefendLineConfig(mode='stack', agent_type='dqn').wrapped_env
+    _sut = doom.VizDoomDefendLineConfig(mode='stack', agent_type='dqn').wrapped_env
     _expected_shape = (96, 128, 3)
 
 
 @unittest.skipUnless(run_tests, reason='ViZDoomGym not installed')
 class TestDoomStackEnvironments(TestPongStackEnvironment):
-    _sut = VizDoomHealthGatheringConfig(mode='stack', agent_type='dqn').wrapped_env
+    _sut = doom.VizDoomHealthGatheringConfig(mode='stack', agent_type='dqn').wrapped_env
     _expected_shape = (96, 128, 3)
 
 
 @unittest.skipUnless(run_tests, reason='ViZDoomGym not installed')
 class TestDoomStackEnvironments(TestPongStackEnvironment):
-    _sut = VizDoomHealthGatheringSupremeConfig(mode='stack', agent_type='dqn').wrapped_env
+    _sut = doom.VizDoomHealthGatheringSupremeConfig(mode='stack', agent_type='dqn').wrapped_env
     _expected_shape = (96, 128, 3)
 
 
 @unittest.skipUnless(run_tests, reason='ViZDoomGym not installed')
 class TestDoomStackEnvironments(TestPongStackEnvironment):
-    _sut = VizDoomMyWayHomeConfig(mode='stack', agent_type='dqn').wrapped_env
+    _sut = doom.VizDoomMyWayHomeConfig(mode='stack', agent_type='dqn').wrapped_env
     _expected_shape = (96, 128, 3)
 
 
 @unittest.skipUnless(run_tests, reason='ViZDoomGym not installed')
 class TestDoomStackEnvironments(TestPongStackEnvironment):
-    _sut = VizDoomPredictPositionConfig(mode='stack', agent_type='dqn').wrapped_env
+    _sut = doom.VizDoomPredictPositionConfig(mode='stack', agent_type='dqn').wrapped_env
     _expected_shape = (90, 160, 3)
 
 
 @unittest.skipUnless(run_tests, reason='ViZDoomGym not installed')
 class TestDoomStackEnvironments(TestPongStackEnvironment):
-    _sut = VizDoomTakeCoverConfig(mode='stack', agent_type='dqn').wrapped_env
+    _sut = doom.VizDoomTakeCoverConfig(mode='stack', agent_type='dqn').wrapped_env
     _expected_shape = (96, 128, 3)
     _show = False
 
 
 @unittest.skipUnless(run_tests, reason='ViZDoomGym not installed')
 class TestDoomStackEnvironments(TestPongDiffEnvironment):
-    _sut = VizDoomBasicConfig(mode='diff', agent_type='dqn').wrapped_env
+    _sut = doom.VizDoomBasicConfig(mode='diff', agent_type='dqn').wrapped_env
     _expected_shape = (96, 128, 1)
 
 
 @unittest.skipUnless(run_tests, reason='ViZDoomGym not installed')
 class TestDoomDiffEnvironments(TestPongDiffEnvironment):
-    _sut = VizDoomCorridorConfig(mode='diff', agent_type='dqn').wrapped_env
+    _sut = doom.VizDoomCorridorConfig(mode='diff', agent_type='dqn').wrapped_env
     _expected_shape = (96, 128, 1)
 
 
 @unittest.skipUnless(run_tests, reason='ViZDoomGym not installed')
 class TestDoomDiffEnvironments(TestPongDiffEnvironment):
-    _sut = VizDoomDeathmatchConfig(mode='diff', agent_type='dqn').wrapped_env
+    _sut = doom.VizDoomDeathmatchConfig(mode='diff', agent_type='dqn').wrapped_env
     _expected_shape = (96, 128, 1)
 
 
 @unittest.skipUnless(run_tests, reason='ViZDoomGym not installed')
 class TestDoomDiffEnvironments(TestPongDiffEnvironment):
-    _sut = VizDoomDefendCenterConfig(mode='diff', agent_type='dqn').wrapped_env
+    _sut = doom.VizDoomDefendCenterConfig(mode='diff', agent_type='dqn').wrapped_env
     _expected_shape = (96, 128, 1)
 
 
 @unittest.skipUnless(run_tests, reason='ViZDoomGym not installed')
 class TestDoomDiffEnvironments(TestPongDiffEnvironment):
-    _sut = VizDoomDefendLineConfig(mode='diff', agent_type='dqn').wrapped_env
+    _sut = doom.VizDoomDefendLineConfig(mode='diff', agent_type='dqn').wrapped_env
     _expected_shape = (96, 128, 1)
 
 
 @unittest.skipUnless(run_tests, reason='ViZDoomGym not installed')
 class TestDoomDiffEnvironments(TestPongDiffEnvironment):
-    _sut = VizDoomHealthGatheringConfig(mode='diff', agent_type='dqn').wrapped_env
+    _sut = doom.VizDoomHealthGatheringConfig(mode='diff', agent_type='dqn').wrapped_env
     _expected_shape = (96, 128, 1)
 
 
 @unittest.skipUnless(run_tests, reason='ViZDoomGym not installed')
 class TestDoomDiffEnvironments(TestPongDiffEnvironment):
-    _sut = VizDoomHealthGatheringSupremeConfig(mode='diff', agent_type='dqn').wrapped_env
+    _sut = doom.VizDoomHealthGatheringSupremeConfig(mode='diff', agent_type='dqn').wrapped_env
     _expected_shape = (96, 128, 1)
 
 
 @unittest.skipUnless(run_tests, reason='ViZDoomGym not installed')
 class TestDoomDiffEnvironments(TestPongDiffEnvironment):
-    _sut = VizDoomMyWayHomeConfig(mode='diff', agent_type='dqn').wrapped_env
+    _sut = doom.VizDoomMyWayHomeConfig(mode='diff', agent_type='dqn').wrapped_env
     _expected_shape = (96, 128, 1)
 
 
 @unittest.skipUnless(run_tests, reason='ViZDoomGym not installed')
 class TestDoomDiffEnvironments(TestPongDiffEnvironment):
-    _sut = VizDoomPredictPositionConfig(mode='diff', agent_type='dqn').wrapped_env
+    _sut = doom.VizDoomPredictPositionConfig(mode='diff', agent_type='dqn').wrapped_env
     _expected_shape = (90, 160, 1)
 
 
 @unittest.skipUnless(run_tests, reason='ViZDoomGym not installed')
 class TestDoomDiffEnvironments(TestPongDiffEnvironment):
-    _sut = VizDoomTakeCoverConfig(mode='diff', agent_type='dqn').wrapped_env
+    _sut = doom.VizDoomTakeCoverConfig(mode='diff', agent_type='dqn').wrapped_env
     _expected_shape = (96, 128, 1)
     _show = False
 
